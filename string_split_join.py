c7611ab25a8ef89e8ce3041f0399b4a45b7dab5d
HarshalKBagul <harshalkbagul1125@gmail.com>
2022-03-01 13:42:23 +0530

    string_split_join

---
+++ string_split_join.py
@@ -0,0 +1,12 @@
+#String splite and Join
+
+#You are given a string. Split the string on a " " 
+#(space) delimiter and join using a - hyphen.
+
+#input:this is a string   
+#output:this-is-a-string
+
+a=input()
+a=a.split()
+a="-".join(a)
+print(a)
