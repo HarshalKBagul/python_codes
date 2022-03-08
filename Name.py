5ce2a175f8f154cdc4aab51eb7c06423d8f03063
HarshalKBagul <harshalkbagul1125@gmail.com>
2022-03-08 17:02:40 +0530

    Name

---
+++ Name.py
@@ -0,0 +1,8 @@
+# whats your name
+
+#You are given the firstname and lastname of a person 
+#on two different lines. Your task is to read them and 
+#print the following:
+
+def print_full_name(a,b):
+    print("Hello "+a+" "+b+" You just delved into python.")
