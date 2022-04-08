d5ddbcace256bee335216daab5aeb9ebad179283
HarshalKBagul <harshalkbagul1125@gmail.com>
2022-04-08 08:24:37 +0530

    time__delta

--- __pycache__/time_delta.py
+++ __pycache__/time_delta.py
@@ -15,8 +15,6 @@ import random
 import re
 import sys
 from datetime import datetime
-
-
 def time_delta(t1, t2):
     time1=datetime.strptime(t1,'%a %d %b %Y %H:%M:%S %z')
     time2=datetime.strptime(t2,'%a %d %b %Y %H:%M:%S %z')
