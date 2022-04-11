024c684337657e0452a3cb2855b5eaa7a7d8d947
HarshalKBagul <harshalkbagul1125@gmail.com>
2022-04-11 07:17:57 +0530

    Maximize_it

---
+++ __pycache__/Maximize_it.py
@@ -0,0 +1,6 @@
+# Maximize it
+
+from itertools import product
+t,m=map(int,input().split())
+l=[list(map(int,input().split())) [1:] for _ in range(t)]
+print(max(map(lambda x: sum(i**2 for i in x)%m, product(*l))))
